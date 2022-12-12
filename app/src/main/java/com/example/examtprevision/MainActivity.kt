3613759189ce05645dc753854d1eb654919c08be
youssefchlendi <youssef.chlendi@gmail.com>
2022-12-12 17:43:17 +0100

    Toast

--- app/src/main/java/com/example/examtprevision/MainActivity.kt
+++ app/src/main/java/com/example/examtprevision/MainActivity.kt
@@ -6,26 +6,12 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    lateinit var spinner : Spinner
-    lateinit var text : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        spinner = findViewById(R.id.spinner)
-        text = findViewById(R.id.text)
+    }
 
-        var arrayAdapter = ArrayAdapter.createFromResource(this,R.array.govs,
-            com.google.android.material.R.layout.support_simple_spinner_dropdown_item)
-        spinner.adapter = arrayAdapter
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                text.text = parent?.getItemAtPosition(position).toString()
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                text.text = "Nothing selected"
-            }
-
-        }
+    fun showToast(view:View?){
+        Toast.makeText(applicationContext,"Message",Toast.LENGTH_LONG).show()
     }
 }
