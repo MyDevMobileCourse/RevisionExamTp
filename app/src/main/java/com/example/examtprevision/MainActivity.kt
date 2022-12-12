c7be6934580bd6c5562faaebc9cc7f6bca964fbf
youssefchlendi <youssef.chlendi@gmail.com>
2022-12-12 16:56:05 +0100

    add edit Text

--- app/src/main/java/com/example/examtprevision/MainActivity.kt
+++ app/src/main/java/com/example/examtprevision/MainActivity.kt
@@ -4,23 +4,31 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     lateinit var myText :TextView
     lateinit var myButton : Button
+    lateinit var myEditText : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         myText = findViewById(R.id.mytext)
         myButton = findViewById(R.id.mybutton)
+        myEditText = findViewById(R.id.myEditText)
         myText.isVisible = false
         myButton.setOnClickListener {
-            myText.setTextColor(Color.BLACK)
-            myText.setBackgroundColor(Color.WHITE)
-            myText.text = "Hello Dsi 31"
+            if(myEditText.text.toString().isBlank()){
+                myText.setTextColor(Color.BLACK)
+                myText.setBackgroundColor(Color.WHITE)
+                myText.text = "Hello Dsi 31"
+            }else{
+                myText.text = myEditText.text.toString()
+            }
             myText.isVisible = true
+
 
         }
 
