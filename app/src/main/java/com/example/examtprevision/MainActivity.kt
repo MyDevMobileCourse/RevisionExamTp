83b347cb906cca4b11770886adfe6289672f5b4f
youssefchlendi <youssef.chlendi@gmail.com>
2022-12-12 18:48:54 +0100

    contextmenu

--- app/src/main/java/com/example/examtprevision/MainActivity.kt
+++ app/src/main/java/com/example/examtprevision/MainActivity.kt
@@ -1,42 +1,37 @@
 package com.example.examtprevision
 
+import android.graphics.Color
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
+import android.view.*
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    lateinit var root : LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        root = findViewById(R.id.root)
+        registerForContextMenu(root)
     }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater : MenuInflater = menuInflater
-        inflater.inflate(R.menu.action_bar,menu)
-        return true;
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        menuInflater.inflate(R.menu.action_bar,menu)
+        menu?.setHeaderTitle("Colors")
+        super.onCreateContextMenu(menu, v, menuInfo)
     }
-
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId){
-            R.id.red -> {
-                Toast.makeText(this, "Red", Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.green -> {
-                Toast.makeText(this, "Green", Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.blue -> {
-                Toast.makeText(this, "Blue", Toast.LENGTH_SHORT).show()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.blue -> root.setBackgroundColor(Color.BLUE)
+            R.id.red -> root.setBackgroundColor(Color.RED)
+            R.id.green -> root.setBackgroundColor(Color.GREEN)
         }
+        return super.onContextItemSelected(item)
     }
+
+
 }
