271c3aa5caa722aa6389b9e168328e3434061238
youssefchlendi <youssef.chlendi@gmail.com>
2022-12-12 19:11:48 +0100

    back and alertdialog

--- app/src/main/java/com/example/examtprevision/MainActivity.kt
+++ app/src/main/java/com/example/examtprevision/MainActivity.kt
@@ -4,40 +4,32 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.*
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     lateinit var root : LinearLayout
-    lateinit var button : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         root = findViewById(R.id.root)
-        button = findViewById(R.id.btn)
+    }
 
-        button.setOnClickListener {
-            val popumenu = PopupMenu(this,button)
-            menuInflater.inflate(R.menu.action_bar,popumenu.menu)
-            popumenu.setOnMenuItemClickListener {
-                when(it.itemId){
-                    R.id.red -> {
-                        root.setBackgroundColor(Color.RED)
-                        true
-                    }
-                    R.id.blue -> {
-                        root.setBackgroundColor(Color.BLUE)
-                        true
-                    }
-                    R.id.green -> {
-                        root.setBackgroundColor(Color.GREEN)
-                        true
-                    }
-                    else -> {false}
-                }
-            }
-                popumenu.show()
-        }
+    override fun onBackPressed() {
+        val alertDialogBuilder = AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Quit");
+        alertDialogBuilder.setMessage("are you sure you want to quit ?  ?");
+        alertDialogBuilder.setPositiveButton("Yes") { dialog, which ->
+            super.onBackPressed()
+        };
+        alertDialogBuilder.setNegativeButton("No") { dialog, which ->
+            Toast.makeText(
+                applicationContext,
+                "Canceled", Toast.LENGTH_SHORT
+            ).show()
+        };
+        alertDialogBuilder.show();
     }
 
 
