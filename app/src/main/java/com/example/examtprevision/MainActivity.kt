507c281576023f93905b90e687f7c94104a39840
youssefchlendi <youssef.chlendi@gmail.com>
2022-12-12 17:10:38 +0100

    checkbox

--- app/src/main/java/com/example/examtprevision/MainActivity.kt
+++ app/src/main/java/com/example/examtprevision/MainActivity.kt
@@ -1,44 +1,34 @@
 package com.example.examtprevision
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
+import android.widget.CheckBox
 import android.widget.TextView
-import androidx.core.view.isVisible
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    lateinit var myText :TextView
-    lateinit var myButton : Button
-    lateinit var myEditText : EditText
-    lateinit var myImage : ImageView
+    lateinit var yes: CheckBox
+    lateinit var no: CheckBox
+    lateinit var text: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        myText = findViewById(R.id.mytext)
-        myButton = findViewById(R.id.mybutton)
-        myEditText = findViewById(R.id.myEditText)
-        myImage = findViewById(R.id.myImage)
-        myText.isVisible = false
-        myImage.isVisible = false
-        myButton.setOnClickListener {
-            if(myEditText.text.toString().isBlank()){
-                myText.setTextColor(Color.BLACK)
-                myText.setBackgroundColor(Color.WHITE)
-                myText.text = "Hello Dsi 31"
-                myImage.setImageResource(R.drawable.ic_launcher_background)
-                myImage.setColorFilter(Color.RED)
-            }else{
-                myText.text = myEditText.text.toString()
-                myImage.setImageResource(R.drawable.ic_launcher_foreground)
-                myImage.setColorFilter(Color.GREEN)
+        yes = findViewById(R.id.yes)
+        no = findViewById(R.id.no)
+        text = findViewById(R.id.result)
 
+        yes.setOnClickListener {
+
+            if (yes.isChecked) {
+                text.text = "I have experience"
+                no.isChecked = false;
             }
-            myText.isVisible = true
-            myImage.isVisible = true
+        }
 
+        no.setOnClickListener {
+            if (no.isChecked) {
+                text.text = "I have no experience"
+                yes.isChecked = false
+            }
 
         }
 
