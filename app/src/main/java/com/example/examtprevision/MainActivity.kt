ed6889cb0fe4e152d4b90b8f096e4b06263b1be1
youssefchlendi <youssef.chlendi@gmail.com>
2022-12-12 16:50:25 +0100

    Button

--- app/src/main/java/com/example/examtprevision/MainActivity.kt
+++ app/src/main/java/com/example/examtprevision/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.examtprevision
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+    lateinit var myText :TextView
+    lateinit var myButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        myText = findViewById(R.id.mytext)
+        myButton = findViewById(R.id.mybutton)
+        myText.isVisible = false
+        myButton.setOnClickListener {
+            myText.setTextColor(Color.BLACK)
+            myText.setBackgroundColor(Color.WHITE)
+            myText.text = "Hello Dsi 31"
+            myText.isVisible = true
+
+        }
+
     }
 }
